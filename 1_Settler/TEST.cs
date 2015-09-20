382108fff2935c7954889a34fa858ddc9938d085
davghouse <davghouse@gmail.com>
2015-09-20 17:17:55 -0400

    PRIME1

--- 1_Settler/TEST.cs
+++ 1_Settler/TEST.cs
@@ -5,7 +5,7 @@ public class TEST
     public static void Main()
     {
         string line;
-        while((line = Console.ReadLine()) != "42")
+        while((line = Console.ReadLine()) != "42" && line != null)
         {
             Console.WriteLine(line);
         }
