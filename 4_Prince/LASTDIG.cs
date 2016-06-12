8987b20ff48781815004b74d8f293a699f0222c8
David Galehouse <davghouse@gmail.com>
2016-06-12 15:41:15 -0400

    Refactor

--- 4_Prince/LASTDIG.cs
+++ 4_Prince/LASTDIG.cs
@@ -12,11 +12,11 @@ public static class LASTDIG
 
 public static class ExponentiatorMod10
 {
-    // Last digit patterns for repeated exponentations of bases 0 through 9.
+    // Last digit patterns for repeated exponentiations of bases 0 through 9.
     // A repeating last digit establishes the pattern, as the next last digit depends only
     // on the last digits being multiplied, not on any digits after, as those just
     // add parts that are divisible by 10 and hence don't affect the mod 10 calculation.
-    private static readonly IReadOnlyList<IReadOnlyList<int>> _bases0To9LastDigitExponentationPatterns;
+    private static readonly IReadOnlyList<IReadOnlyList<int>> _bases0To9LastDigitExponentiationPatterns;
 
     static ExponentiatorMod10()
     {
@@ -31,7 +31,7 @@ public static class ExponentiatorMod10
         var base8Pattern = new int[] { 8, 4, 2, 6 }; // 8, 64, 512, 4096, (32768)
         var base9Pattern = new int[] { 9, 1 }; // 9, 81, (729)
 
-        _bases0To9LastDigitExponentationPatterns = new int[][]
+        _bases0To9LastDigitExponentiationPatterns = new int[][]
         {
             base0Pattern, base1Pattern, base2Pattern, base3Pattern, base4Pattern,
             base5Pattern, base6Pattern, base7Pattern, base8Pattern, base9Pattern
@@ -47,17 +47,17 @@ public static class ExponentiatorMod10
         // where (b - d) is divisible by 10 (0 mod 10), so only the d^e term in the expansion matters.
         int d = b % 10;
 
-        var lastDigitExponentationPattern = _bases0To9LastDigitExponentationPatterns[d];
+        var lastDigitExponentiationPattern = _bases0To9LastDigitExponentiationPatterns[d];
 
-        // Pattern starts at exponent of 1 and ends at exponent of lastDigitExponentationPattern.Count,
+        // Pattern starts at exponent of 1 and ends at exponent of lastDigitExponentiationPattern.Count,
         // so e mod the count almost gives the correct position, just have to move zero back to the end.
-        int patternPosition = e % lastDigitExponentationPattern.Count;
+        int patternPosition = e % lastDigitExponentiationPattern.Count;
         if (patternPosition == 0)
         {
-            patternPosition = lastDigitExponentationPattern.Count;
+            patternPosition = lastDigitExponentiationPattern.Count;
         }
 
-        return lastDigitExponentationPattern[patternPosition - 1];
+        return lastDigitExponentiationPattern[patternPosition - 1];
     }
 }
 
