cba2ab460ff9db79d1e850a4a4d525418fea39c2
David Galehouse <davghouse@gmail.com>
2016-06-19 16:12:15 -0400

    CANDY3

---
+++ Spoj.Solver/Solutions/4_Prince/CANDY3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Candy III
+// 2148 http://www.spoj.com/problems/CANDY3/
+// Determines if N bags full of candies can have their contents redistributed equally amongst N children.
+public static class TEMPLATE
+{
+    // I guess the numbers will be too big if summed directly, so need to use property of modular arithmetic:
+    // (a + b) mod m == (a mod m + b mod m) mod m
+    public static string Solve(ulong[] backpackCandyCounts)
+    {
+        uint N = (uint)backpackCandyCounts.Count;
+        ulong modSum = 0;
+
+        for (int i = 0; i < N; ++i)
+        {
+            modSum = (modSum + (backpackCandyCounts[i] % N)) % N;
+        }
+
+        return modSum == 0 ? "YES" : "NO";
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+
+        while (remainingTestCases-- > 0)
+        {
+            Console.ReadLine(); // Remove blank line.
+
+            int backpackCount = int.Parse(Console.ReadLine());
+            ulong[] backpackCandyCounts = new ulong[backpackCount];
+
+            for (int i = 0; i < backpackCount; ++i)
+            {
+                backpackCandyCounts[i] = ulong.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(
+                TEMPLATE.Solve(backpackCandyCounts));
+        }
+    }
+}
