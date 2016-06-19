a98d577c63f5cf88e09e77991a3eb52ebd22e5b4
David Galehouse <davghouse@gmail.com>
2016-06-19 12:00:10 -0400

    COINS refactor
    Get rid of the infrastructure that was necessary when all 500 million
    values were cached and memory was a concern. Experiment to find a good
    limit for the cached values.

--- Spoj.Solver/Solutions/5_King/COINS.cs
+++ Spoj.Solver/Solutions/5_King/COINS.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 // COINS
 // 346 http://www.spoj.com/problems/COINS/
@@ -7,53 +6,28 @@ using System.Collections.Generic;
 // or split the coins up as Byteland banks allow (n/2, n/3, n/4 coins and then recursively on those...).
 public static class COINS
 {
-    private const int _limit = 1000000000;
-    private const int _cachedLimit = 976562; // 1 billion / 2^10
+    private const int _cachedLimit = 15258; // 1 billion / 2^16
 
-    // Roughly, given a coin of value n, look up its exchange value index in the indices list.
-    // Then, use the index to find the exchange value from the values list. Not enough memory
-    // to store longs for each n, so store shorts that map to longs (turns out the number of
-    // distinct values is on the order of only 1000). The mapper facilitates working with these
-    // two lists; after calculating the value, look up the short which indexes the value in the values list.
-    // And on top of that, to calculate all solutions only need up to limit/2 (for the n/2 split).
-    // ** Actually, doing it up to limit/2 is way too slow. Do it partially on some handpicked value,
-    // which doesn't take long but trims out enough of the exponentially growing recursion to make it fast.
-    private static short[] _exchangeValueIndices;
-    private static List<long> _exchangeValues;
-    private static Dictionary<long, short> _exchangeValueIndexMapper;
+    // Cache some of the first 500 million values necessary to do this without recursive calls.
+    // The limit for the cache was picked through experimentation; doesn't take too long to
+    // create the cache, and trims the exponentially growing recursion enough to be fast for the problem's input.
+    private static long[] _exchangeValues;
 
     static COINS()
     {
-        _exchangeValueIndices = new short[_cachedLimit + 1];
-        _exchangeValues = new List<long>();
-        _exchangeValueIndexMapper = new Dictionary<long, short>();
-
-        _exchangeValueIndices[0] = 0;
-        _exchangeValues.Add(0);
-        _exchangeValueIndexMapper.Add(0, 0);
+        _exchangeValues = new long[_cachedLimit + 1];
+        _exchangeValues[0] = 0;
 
         for (int n = 1; n <= _cachedLimit; ++n)
         {
-            long exchangeValue = Math.Max(n, GetExchangeValue(n / 2) + GetExchangeValue(n / 3) + GetExchangeValue(n / 4));
-
-            short exchangeValueIndex;
-            if (!_exchangeValueIndexMapper.TryGetValue(exchangeValue, out exchangeValueIndex))
-            {
-                // Haven't seen this exchange value before, so add it to the list and store its index
-                // in the value index mapper so it's easy to find for other n's with this value.
-                _exchangeValues.Add(exchangeValue);
-                exchangeValueIndex = (short)(_exchangeValues.Count - 1);
-                _exchangeValueIndexMapper.Add(exchangeValue, exchangeValueIndex);
-            }
-
-            _exchangeValueIndices[n] = exchangeValueIndex;
+            _exchangeValues[n] = Math.Max(n, GetExchangeValue(n / 2) + GetExchangeValue(n / 3) + GetExchangeValue(n / 4));
         }
     }
 
     private static long GetExchangeValue(int n)
     {
         if (n <= _cachedLimit)
-            return _exchangeValues[_exchangeValueIndices[n]];
+            return _exchangeValues[n];
 
         return Math.Max(n, GetExchangeValue(n / 2) + GetExchangeValue(n / 3) + GetExchangeValue(n / 4));
     }
