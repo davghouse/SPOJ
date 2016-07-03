511bff092534f1be5a744597f47af65ac2f57539
David Galehouse <davghouse@gmail.com>
2016-07-02 22:33:12 -0400

    ACODE

---
+++ Spoj.Solver/Solutions/5_King/ACODE_v1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Alphacode v1 (top-down, recursion with memoization)
+// 394 http://www.spoj.com/problems/ACODE/
+// Returns the number of ways to decode a number (string of digits) representing characters in alphabetical position.
+public static class ACODE
+{
+    private static IReadOnlyDictionary<string, long> _codedLetterDecodeCounts = new Dictionary<string, long>
+    {
+        { "1", 1 }, { "2", 1 }, { "3", 1 }, { "4", 1 }, { "5", 1 }, { "6", 1 }, { "7", 1 },
+        { "8", 1 }, { "9", 1 }, { "10", 1 }, { "11", 2 }, { "12", 2 }, { "13", 2 }, { "14", 2},
+        { "15", 2 }, { "16", 2 }, { "17", 2 }, { "18", 2 }, { "19", 2 }, { "20", 1 }, { "21", 2 },
+        { "22", 2 }, { "23", 2 }, { "24", 2 }, { "25", 2 }, { "26", 2 }
+    };
+
+    private static bool IsNumberACodedLetter(string number)
+        => _codedLetterDecodeCounts.ContainsKey(number);
+
+    private static long CodedLetterDecodeCount(string codedLetter)
+        => _codedLetterDecodeCounts[codedLetter];
+
+    public static long Solve(string number)
+    {
+        Solver s = new Solver(number);
+        return s.Solve();
+    }
+
+    private class Solver
+    {
+        private string _number;
+        // The two way branching of the recursion causes us to call Solve on the same number many times; use this dictionary to memoize.
+        // Depth of recursion/size of the dictionary only grows to about the length of the input number, which is limited to 5000.
+        private Dictionary<string, long> _decodeCounts = new Dictionary<string, long>();
+
+        public Solver(string numbers)
+        {
+            _number = numbers;
+        }
+
+        public long Solve()
+            => Solve(_number);
+
+        private long Solve(string number)
+        {
+            // Base case could be number.Length == 0 return 1, but this was convenient.
+            if (IsNumberACodedLetter(number))
+                return CodedLetterDecodeCount(number);
+
+            long decodeCount;
+            if (_decodeCounts.TryGetValue(number, out decodeCount))
+                return decodeCount;
+
+            bool firstDigitCanBeLetter = IsNumberACodedLetter(number.Substring(0, 1));
+            bool firstTwoDigitsCanBeLetter = number.Length >= 2 && IsNumberACodedLetter(number.Substring(0, 2));
+
+            if (firstDigitCanBeLetter)
+            {
+                string numberAfterFirstDigit = number.Substring(1);
+                decodeCount += Solve(numberAfterFirstDigit);
+            }
+            if (firstTwoDigitsCanBeLetter)
+            {
+                string numberAfterFirstTwoDigits = number.Substring(2);
+                decodeCount += Solve(numberAfterFirstTwoDigits);
+            }
+
+            _decodeCounts.Add(number, decodeCount);
+            return decodeCount;
+        }
+
+        // Cuts the number of calls to Solve by a lot but I guess it's less natural/doesn't read as well/has code duplication.
+        //private long Solve(string number)
+        //{
+        //    if (IsNumberACodedLetter(number))
+        //        return CodedLetterDecodeCount(number);
+
+        //    bool firstDigitCanBeLetter = IsNumberACodedLetter(number.Substring(0, 1));
+        //    bool firstTwoDigitsCanBeLetter = number.Length >= 2 && IsNumberACodedLetter(number.Substring(0, 2));
+
+        //    long decodeCount = 0;
+        //    if (firstDigitCanBeLetter)
+        //    {
+        //        string numberAfterFirstDigit = number.Substring(1);
+        //        long decodeCountOfNumberAfterFirstDigit;
+        //        if (!_decodeCounts.TryGetValue(numberAfterFirstDigit, out decodeCountOfNumberAfterFirstDigit))
+        //        {
+        //            decodeCountOfNumberAfterFirstDigit = Solve(numberAfterFirstDigit);
+        //            _decodeCounts.Add(numberAfterFirstDigit, decodeCountOfNumberAfterFirstDigit);
+        //        }
+
+        //        decodeCount += decodeCountOfNumberAfterFirstDigit;
+        //    }
+        //    if (firstTwoDigitsCanBeLetter)
+        //    {
+        //        string numberAfterFirstTwoDigits = number.Substring(2);
+        //        long decodeCountOfNumberAfterFirstTwoDigits;
+        //        if (!_decodeCounts.TryGetValue(numberAfterFirstTwoDigits, out decodeCountOfNumberAfterFirstTwoDigits))
+        //        {
+        //            decodeCountOfNumberAfterFirstTwoDigits = Solve(numberAfterFirstTwoDigits);
+        //            _decodeCounts.Add(numberAfterFirstTwoDigits, decodeCountOfNumberAfterFirstTwoDigits);
+        //        }
+
+        //        decodeCount += decodeCountOfNumberAfterFirstTwoDigits;
+        //    }
+
+        //    return decodeCount;
+        //}
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        string number;
+        while ((number = Console.ReadLine()) != "0")
+        {
+            Console.WriteLine(
+                ACODE.Solve(number));
+        }
+    }
+}
---
+++ Spoj.Solver/Solutions/5_King/ACODE_v2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// Alphacode v2 (bottom-up, dynamic programming with tabulation)
+// 394 http://www.spoj.com/problems/ACODE/
+// Returns the number of ways to decode a number (string of digits) representing characters in alphabetical position.
+public static class ACODE
+{
+    // Array that's built dynamically, containing at index i the decode count for the first i digits
+    // of the given number. The number is guaranteed to have no more than 5000 digits.
+    private static long[] _decodeCounts = new long[5001];
+
+    public static long Solve(string number)
+    {
+        // [0] is 1 as a base case for no digits, [1] is 1 as the first digit always decodes in a single way.
+        _decodeCounts[0] = _decodeCounts[1] = 1;
+
+        // _decodeCounts index is one-based, number is zero-based, kind of annoying but this looks the cleanest.
+        for (int ni = 1, di = 2; ni < number.Length; ++ni, ++di)
+        {
+            // If the digit is 0, it has to be used with the previous digit; 0 can only end a coded letter.
+            // Hence, the previous digit plus it create one letter that gets bolted on to however many valid
+            // decodings are formed by the first di - 2 digits.
+            if (number[ni] == '0')
+            {
+                _decodeCounts[di] = _decodeCounts[di - 2];
+            }
+            // The digit isn't zero, so it can be translated to a number by itself. Hence, it can be bolted
+            // on to however many valid decodings are formed by the first di - 1 digits.
+            else
+            {
+                _decodeCounts[di] = _decodeCounts[di - 1];
+
+                // ...But it might also combine with the previous digit to form a decoded letter, in which case those two
+                // together could get bolted on to however many valid decodings there are for the first di - 2 digits.
+                if (number[ni - 1] == '1' || number[ni - 1] == '2' && number[ni] <= '6') // 1[1-9] || 2[1-6] is valid.
+                {
+                    _decodeCounts[di] += _decodeCounts[di - 2];
+                }
+            }
+        }
+
+        return _decodeCounts[number.Length];
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        string number;
+        while ((number = Console.ReadLine()) != "0")
+        {
+            Console.WriteLine(
+                ACODE.Solve(number));
+        }
+    }
+}
