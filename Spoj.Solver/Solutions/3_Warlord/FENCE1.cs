fcc9be52f761d3d8815a69acbbd1adb4b865619e
David Galehouse <davghouse@gmail.com>
2016-07-09 23:02:25 -0400

    FENCE1

--- Spoj.Solver/Solutions/3_Warlord/FENCE1.cs
+++ Spoj.Solver/Solutions/3_Warlord/FENCE1.cs
@@ -8,8 +8,8 @@ public static class FENCE1
     // So the answer here is to make a half circle with the fence, the wall forming the straight side.
     // Not sure how to prove it but it makes sense intuitively; convex, uses a lot of the free wall.
     // For the whole circle, C/2 = pi*r = length => r = length/pi, => A/2 = pi * r^2 / 2 = length ^ 2 / (2 * pi).
-    public static string Solve(int length)
-        => (length * length / (2 * Math.PI)).ToString("F2");
+    public static double Solve(int length)
+        => length * length / (2 * Math.PI);
 }
 
 public static class Program
@@ -20,7 +20,7 @@ public static class Program
         while ((length = int.Parse(Console.ReadLine())) != 0)
         {
             Console.WriteLine(
-                FENCE1.Solve(length));
+                FENCE1.Solve(length).ToString("F2"));
         }
     }
 }
