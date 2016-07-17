e334386e9c925fe54e63443fe095be78cdf183f2
David Galehouse <davghouse@gmail.com>
2016-07-17 01:11:02 -0400

    Input generator

---
+++ Spoj.Library.Tests/InputGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Spoj.Library.Tests
+{
+    [TestClass]
+    public class InputGeneratorTests
+    {
+        [TestMethod]
+        public void DoesntGenerateDistinctRandomInts()
+        {
+            int[] ints = InputGenerator.GenerateRandomInts(10000, 0, 9999);
+
+            Assert.IsTrue(!ints.SequenceEqual(ints.Distinct()));
+        }
+
+        [TestMethod]
+        public void GeneratesDistinctRandomInts()
+        {
+            int[] ints = InputGenerator.GenerateDistinctRandomInts(10000, 0, 10000);
+
+            Assert.IsTrue(ints.SequenceEqual(ints.Distinct()));
+        }
+    }
+}
---
+++ Spoj.Library/InputGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spoj.Library
+{
+    public static class InputGenerator
+    {
+        // Inclusive minValue, exclusive maxValue.
+        public static int[] GenerateRandomInts(int count, int minValue = 0, int maxValue = int.MaxValue)
+        {
+            var rand = new Random();
+
+            int[] ints = new int[count];
+            for (int i = 0; i < count; ++i)
+            {
+                ints[i] = rand.Next(minValue, maxValue);
+            }
+
+            return ints;
+        }
+
+        // Inclusive minValue, exclusive maxValue.
+        public static int[] GenerateDistinctRandomInts(int count, int minValue = 0, int maxValue = int.MaxValue)
+        {
+            var rand = new Random();
+
+            var distinctRandomInts = new HashSet<int>();
+            int[] ints = new int[count];
+            while (distinctRandomInts.Count < count)
+            {
+                int randInt = rand.Next(minValue, maxValue);
+                if (distinctRandomInts.Add(randInt))
+                {
+                    ints[distinctRandomInts.Count - 1] = randInt;
+                }
+            }
+
+            return ints;
+        }
+    }
+}
