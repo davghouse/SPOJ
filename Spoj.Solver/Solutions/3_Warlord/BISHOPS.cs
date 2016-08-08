ecd4d26318160a280581ff53a7f8ed30033c1d5b
David Galehouse <davghouse@gmail.com>
2016-08-08 00:51:08 -0400

    BISHOPS

---
+++ Spoj.Solver/Solutions/3_Warlord/BISHOPS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+// 328 http://www.spoj.com/problems/BISHOPS/ Bishops
+// Finds the maximum number of bishops that can be (safely) placed on a given-size chessboard.
+public static class BISHOPS
+{
+    // I didn't prove optimality but it's easy to show at least 2n - 2 bishops is always
+    // possible by placing n along the top and n - 2 along the base (none in the corners).
+    // Might be easy to show 2n isn't possible.
+    public static BigInteger Solve(BigInteger size)
+        => size == 1 ? 1 : 2*size - 2;
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            Console.WriteLine(
+                BISHOPS.Solve(BigInteger.Parse(line)));
+        }
+    }
+}
