7ae903996db9473b70f1ad50ca0b282dfa2019bf
David Galehouse <davghouse@gmail.com>
2016-08-08 21:37:09 -0400

    OFFSIDE

---
+++ Spoj.Solver/Solutions/3_Warlord/OFFSIDE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+// 2178 http://www.spoj.com/problems/OFFSIDE/ He is offside!
+// Determines if an attacker is offsides (using player positions only).
+public static class OFFSIDE
+{
+    public static string Solve(int[] attackers, int[] defenders)
+    {
+        // There may be attackers further than the closest attacker who are also offsides,
+        // but that doesn't matter here. If anyone's offsides, the closest attacker is too.
+        int closestAttacker = attackers.Min();
+
+        // Rather than finding the closest and second closest defenders, note that the
+        // attacker is offsides if he is cleanly before all the defenders except at most one.
+        int defendersFurtherThanClosestAttacker = defenders.Count(d => d > closestAttacker);
+
+        return defendersFurtherThanClosestAttacker >= defenders.Length - 1 ? "Y" : "N";
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int[] line;
+        while ((line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse))[0] != 0)
+        {
+            int[] attackers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] defenders = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(
+                OFFSIDE.Solve(attackers, defenders));
+        }
+    }
+}
