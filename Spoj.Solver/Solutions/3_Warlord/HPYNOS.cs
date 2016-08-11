ca221575c1901961b9919026c8ceed94e4c86cb7
David Galehouse <davghouse@gmail.com>
2016-08-10 21:56:43 -0400

    HPYNOS

---
+++ Spoj.Solver/Solutions/3_Warlord/HPYNOS.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+// 7733 http://www.spoj.com/problems/HPYNOS/ Happy Numbers I
+// Determines if repeatedly adding the squared digits of a number eventually gets to 1.
+public static class HPYNOS
+{
+    // n is limited to 2,147,483,647, so by inspection 1,999,999,999 will give us the
+    // limit of the numbers we can get after at least one breaking. That is:
+    // 1^2 + 9 * 9^2 = 730. So we'll have a size 731 bit array to keep track of already seen.
+    public static int Solve(int n)
+    {
+        BitArray numbersAlreadySeen = new BitArray(731);
+        int breakCount = 0;
+
+        while (n != 1)
+        {
+            n = Break(n);
+            ++breakCount;
+
+            if (numbersAlreadySeen[n])
+                return -1;
+            else
+                numbersAlreadySeen[n] = true;
+        }
+
+        return breakCount;
+    }
+
+    private static int Break(int n)
+    {
+        int result = 0;
+
+        while (n != 0)
+        {
+            int lastDigit = n % 10;
+            result += lastDigit * lastDigit;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine(
+            HPYNOS.Solve(int.Parse(Console.ReadLine())));
+    }
+}
