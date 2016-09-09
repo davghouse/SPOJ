bb6313bdd28148cd8d09f62272dce7236d14434c
David Galehouse <davghouse@gmail.com>
2016-09-08 23:24:11 -0400

    TRT

---
+++ Spoj.Solver/Solutions/5_King/TRT_v1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 740 http://www.spoj.com/problems/TRT/ Treats for the Cows
+// Finds the optimal order to sell cow treats that become more valuable over time.
+public static class TRT // v1, top-down, recursion with memoization
+{
+    private struct IndexRange
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public static IndexRange Create(int startIndex, int endIndex)
+            => new IndexRange
+            {
+                Start = startIndex,
+                End = endIndex,
+            };
+    }
+
+    // Observation: for a given range, the starting age is always the same. That's because the starting age
+    // corresponds to the number of treats chosen before arriving at the range, and to arrive at a range
+    // we must choose whatever's to the left of it and whatever's to the right of it, and that's the only way.
+    // The order those treats outside the range were chosen can differ, but that doesn't affect the range's starting age.
+    // So the memoization doesn't need to worry about age (this was realized after studying the non-memoized recursive sol'n).
+    // Unfortunately memoization gives a TLE, but it helps motivate a bottom-up DP in TRT_v2.
+    public static int Solve(int[] treatValues)
+        => SolveWithMemoization(
+            treatValues,
+            IndexRange.Create(0, treatValues.Length - 1),
+            age: 1,
+            memoizer: new Dictionary<IndexRange, int>());
+
+    private static int SolveWithMemoization(
+        int[] treatValues, IndexRange indexRange, int age, Dictionary<IndexRange, int> memoizer)
+    {
+        if (indexRange.Start == indexRange.End)
+            return age * treatValues[indexRange.Start];
+
+        int result;
+        if (memoizer.TryGetValue(indexRange, out result))
+            return result;
+
+        result = Math.Max(
+            age * treatValues[indexRange.Start]
+            + SolveWithMemoization(treatValues, IndexRange.Create(indexRange.Start + 1, indexRange.End), age + 1, memoizer),
+            age * treatValues[indexRange.End]
+            + SolveWithMemoization(treatValues, IndexRange.Create(indexRange.Start, indexRange.End - 1), age + 1, memoizer));
+
+        memoizer.Add(indexRange, result);
+
+        return result;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int treatCount = int.Parse(Console.ReadLine());
+        var treatValues = new int[treatCount];
+
+        for (int i = 0; i < treatCount; ++i)
+        {
+            treatValues[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(TRT.Solve(treatValues));
+    }
+}
---
+++ Spoj.Solver/Solutions/5_King/TRT_v2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+// 740 http://www.spoj.com/problems/TRT/ Treats for the Cows
+// Finds the optimal order to sell cow treats that become more valuable over time.
+public static class TRT // v2, bottom-up, dynamic programming with tabulation
+{
+    private static int[,] maximumRevenues;
+
+    // See TRT_v1 for the memoized solution that helps motivate this DP.
+    // A 2D array is used for the DP, where the first index corresponds to a range's start index
+    // and the second index corresponds to that range's end index. The value in the array isn't just
+    // the maximum revenue of range, it's the maximum revenue given that the requisite number of days has
+    // passed to allow us to choose all the treats outside the range. Details:
+    //
+    // For the range from i (row) to j (column), i <= j (below the diagonal is N/A, as not valid ranges),
+    // the maximum revenue for that range (given the starting age is i + ((treatCount.Length - 1) - j) + 1) is
+    // = maximumRevenues[i, j]
+    // = Max(age * treatValues[i] + maximumRevenues[i + 1, j], age * treatValues[j] + maximumRevenues[i, j - 1]).
+    // First corresponds to choosing the first treat in the range, the second to choosing the last treat, our only options.
+    // So we need to fill the maximumRevenues table such that the value underneath and the value to the left are already calculated.
+    public static int Solve(int treatCount, int[] treatValues)
+    {
+        maximumRevenues = new int[treatCount, treatCount];
+
+        // Initialize along the diagonal.
+        for (int d = 0; d < treatCount; ++d)
+        {
+            // Starting age of a single treat is always treatCount, but call the method here to be explicit.
+            maximumRevenues[d, d] = StartingAgeOfRange(d, d, treatCount) * treatValues[d];
+        }
+
+        // While moving right across the columns, solve up from the diagonal.
+        for (int c = 1; c < treatCount; ++c)
+        {
+            for (int r = c - 1; r >= 0; --r)
+            {
+                int startingAgeOfRange = StartingAgeOfRange(r, c, treatCount);
+
+                maximumRevenues[r, c] = Math.Max(
+                    startingAgeOfRange * treatValues[r] + maximumRevenues[r + 1, c],
+                    startingAgeOfRange * treatValues[c] + maximumRevenues[r, c - 1]);
+            }
+        }
+
+        // Return the revenue for the full range, which is in the top right corner of the table.
+        return maximumRevenues[0, treatCount - 1];
+    }
+
+    // Note the full range has an age of 1 (the multiplier for the first treat sold is 1).
+    // Counting treats outside of the range, as that corresponds to days until the range is arrived at: ----[ ... ]--- => 7.
+    private static int StartingAgeOfRange(int startIndex, int endIndex, int treatCount)
+        // startIndex + ((treatCount - 1) - endIndex) + 1 =>
+        => startIndex + treatCount - endIndex;
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int treatCount = int.Parse(Console.ReadLine());
+        var treatValues = new int[treatCount];
+
+        for (int i = 0; i < treatCount; ++i)
+        {
+            treatValues[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(TRT.Solve(treatCount, treatValues));
+    }
+}
