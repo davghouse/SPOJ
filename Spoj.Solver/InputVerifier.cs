fdf74c2a4b707cafffd48cc40dabba8738740eed
David Galehouse <davghouse@gmail.com>
2016-09-21 23:45:53 -0400

    DOTAA, InputVerifier concept

---
+++ Spoj.Solver/InputVerifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Too many problems have improperly formatted input... random whitespace,
+// missing newlines... things that C# doesn't deal well with by default.
+// It's good to verify the input format on an alt account so you don't get frustrated.
+// Currently verifying for: http://www.spoj.com/problems/DOTAA/
+public static class InputVerifier
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+
+        while (remainingTestCases-- > 0)
+        {
+            int[] line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            if (line.Length != 3)
+            {
+                throw new FormatException();
+            }
+
+            for (int i = 0; i < line[0]; ++i)
+            {
+                int a = int.Parse(Console.ReadLine());
+            }
+            Console.ReadLine(); // Don't need this; there's actually no newline between test cases.
+        }
+    }
+}
---
+++ Spoj.Solver/Solutions/4_Prince/DOTAA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// 10286 http://www.spoj.com/problems/DOTAA/ DOTA HEROES
+// Figures out if the heroes can make it past the towers without anyone dying.
+public static class DOTAA
+{
+    // All towers do the same damage. Each hero can tank a certain amount of
+    // tower hits before dying. We can calculate how many survivable hits for
+    // each individual hero as they are read, then compare that total to the tower
+    // count to see if it's possible for all heroes to survive.
+    public static string Solve(int heroCount, int towerCount, int towerDamage)
+    {
+        int totalSurvivableHits = 0;
+
+        for (int h = 0; h < heroCount; ++h)
+        {
+            int heroHealth = int.Parse(Console.ReadLine());
+
+            // Divide the expendable health by the tower damage (and round down, implicitly).
+            totalSurvivableHits += (heroHealth - 1) / towerDamage;
+        }
+
+        return totalSurvivableHits >= towerCount ? "YES" : "NO";
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+
+        while (remainingTestCases-- > 0)
+        {
+            int[] line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Console.WriteLine(
+                DOTAA.Solve(heroCount: line[0], towerCount: line[1], towerDamage: line[2]));
+        }
+    }
+}
