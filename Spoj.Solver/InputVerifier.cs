b075d7265254b40ff58ed88214d4f011e1e2bd76
David Galehouse <davghouse@gmail.com>
2016-10-02 17:18:00 -0400

    FACEFRND

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -3,33 +3,27 @@
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/BUGLIFE/
+// Currently verifying for: http://www.spoj.com/problems/FACEFRND/
 public static class InputVerifier
 {
     private static void Main()
     {
-        int remainingTestCases = int.Parse(Console.ReadLine());
+        int friendCount = int.Parse(Console.ReadLine());
 
-        while (remainingTestCases-- > 0)
+        if (friendCount < 1 || friendCount > 100)
+            throw new FormatException();
+
+        for (int i = 0; i < friendCount; ++i)
         {
             int[] line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int friendID = line[0];
+            int friendFriendCount = line[1];
 
-            if (line.Length != 2)
+            if (friendFriendCount < 1 || friendFriendCount > 100)
                 throw new FormatException();
 
-            int bugCount = line[0];
-            int interactionCount = line[1];
-
-            for (int i = 0; i < interactionCount; ++i)
-            {
-                line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-                if (line.Length != 2)
-                    throw new FormatException();
-
-                if (line[0] > bugCount || line[1] > bugCount)
-                    throw new FormatException();
-            }
+            if (line.Length != friendFriendCount + 2)
+                throw new FormatException();
         }
     }
 }
---
+++ Spoj.Solver/Solutions/4_Prince/FACEFRND.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// 9788 http://www.spoj.com/problems/FACEFRND/ Friends of Friends
+// Finds friends of someone's friends given their friends and their friends' friends.
+public static class FACEFRND
+{
+    public static int SolveWithHashSets(int friendCount, int[][] friendDefinitions)
+    {
+        var friends = new HashSet<int>();
+        for (int f = 0; f < friendCount; ++f)
+        {
+            friends.Add(friendDefinitions[f][0]);
+        }
+
+        var friendsOfFriends = new HashSet<int>();
+        for (int f = 0; f < friendCount; ++f)
+        {
+            int friendsOfFriendsCount = friendDefinitions[f][1];
+            for (int fof = 0; fof < friendsOfFriendsCount; ++fof)
+            {
+                int friendOfFriendID = friendDefinitions[f][fof + 2];
+
+                if (!friends.Contains(friendOfFriendID))
+                {
+                    friendsOfFriends.Add(friendOfFriendID);
+                }
+            }
+        }
+
+        return friendsOfFriends.Count;
+    }
+
+    public static int SolveWithExcept(int friendCount, int[][] friendDefinitions)
+    {
+        var friends = new List<int>();
+        var friendsOfFriends = new List<int>();
+
+        for (int f = 0; f < friendCount; ++f)
+        {
+            friends.Add(friendDefinitions[f][0]);
+
+            int friendsOfFriendsCount = friendDefinitions[f][1];
+            for (int fof = 0; fof < friendsOfFriendsCount; ++fof)
+            {
+                friendsOfFriends.Add(friendDefinitions[f][fof + 2]);
+            }
+        }
+
+        return friendsOfFriends.Except(friends).Count();
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int friendCount = int.Parse(Console.ReadLine());
+        int[][] friendDefinitions = new int[friendCount][];
+
+        for (int f = 0; f < friendCount; ++f)
+        {
+            friendDefinitions[f] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        }
+
+        Console.WriteLine(
+            FACEFRND.SolveWithExcept(friendCount, friendDefinitions));
+    }
+}
