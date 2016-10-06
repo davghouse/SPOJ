8154438e4fec1f70845b4b71b875cbfe1a87322e
David Galehouse <davghouse@gmail.com>
2016-10-05 23:44:13 -0400

    ANARC05B

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -1,26 +1,50 @@
 ﻿using System;
+using System.Linq;
 
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/TWOSQRS/
+// Currently verifying for: http://www.spoj.com/problems/ANARC05B/ (it's improperly formatted)
 public static class InputVerifier
 {
-    private const long _oneTrillion = 1000000000000;
-
     private static void Main()
     {
-        int remainingTestCases = int.Parse(Console.ReadLine());
+        int[] firstSequence;
+        int[] secondSequence;
 
-        if (remainingTestCases < 1 || remainingTestCases > 100)
-            throw new FormatException();
-
-        while (remainingTestCases-- > 0)
+        while ((firstSequence = Array.ConvertAll(
+            Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+            int.Parse)).Length != 1)
         {
-            long n = long.Parse(Console.ReadLine());
+            secondSequence = Array.ConvertAll(
+                Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+                int.Parse);
 
-            if (n < 0 || n > _oneTrillion)
+            if (firstSequence[0] != firstSequence.Length - 1
+                || firstSequence[0] > 10000)
                 throw new FormatException();
+
+            if (secondSequence[0] != secondSequence.Length - 1
+                || secondSequence[0] > 10000)
+                throw new FormatException();
+
+            if (firstSequence.Any(v => v < -10000 || v > 10000))
+                throw new FormatException();
+
+            if (secondSequence.Any(v => v < -10000 || v > 10000))
+                throw new FormatException();
+
+            for (int i = 2; i < firstSequence.Length; ++i)
+            {
+                if (firstSequence[i - 1] >= firstSequence[i])
+                    throw new FormatException();
+            }
+
+            for (int i = 2; i < secondSequence.Length; ++i)
+            {
+                if (secondSequence[i - 1] >= secondSequence[i])
+                    throw new FormatException();
+            }
         }
     }
 }
---
+++ Spoj.Solver/Solutions/5_King/ANARC05B.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+// 4560 http://www.spoj.com/problems/ANARC05B/ The Double HeLiX
+// Maximizes the sum while traversing a pair of intersecting, ordered sequences.
+public static class ANARC05B
+{
+    // Sequences are effectively one-indexed as the first element is the sequence size.
+    // And they're strictly increasing. And there's no cost for jumping between sequences,
+    // so we can just do this greedily. The sequence to use from each intersection point
+    // is the one whose sum until the next intersection point is greatest.
+    public static int Solve(int[] firstSequence, int[] secondSequence)
+    {
+        int firstSequenceLength = firstSequence[0];
+        int secondSequenceLength = secondSequence[0];
+        var intersectionPoints = new Queue<Tuple<int, int>>();
+
+        for (int fi = 1; fi <= firstSequenceLength; ++fi)
+        {
+            // Could save work by only searching from the previous intersection point (w/o queue), nah.
+            int si = Array.BinarySearch(secondSequence, 1, secondSequenceLength, firstSequence[fi]);
+            if (si > 0)
+            {
+                intersectionPoints.Enqueue(Tuple.Create(fi, si));
+            }
+        }
+
+        int maximumSum = 0;
+        int indexSummedThroughForFirstSequence = 0;
+        int indexSummedThroughForSecondSequence = 0;
+
+        while (intersectionPoints.Count > 0)
+        {
+            var intersectionPoint = intersectionPoints.Dequeue();
+
+            maximumSum += Math.Max(
+                GetSumBetween(firstSequence,
+                    startIndex: indexSummedThroughForFirstSequence + 1,
+                    endIndex: intersectionPoint.Item1),
+                GetSumBetween(secondSequence,
+                    startIndex: indexSummedThroughForSecondSequence + 1,
+                    endIndex: intersectionPoint.Item2));
+
+            indexSummedThroughForFirstSequence = intersectionPoint.Item1;
+            indexSummedThroughForSecondSequence = intersectionPoint.Item2;
+        }
+
+        // One more choice: should we fall off the end of the first or the second (if we haven't already)?
+        maximumSum += Math.Max(
+            GetSumBetween(firstSequence,
+                startIndex: indexSummedThroughForFirstSequence + 1,
+                endIndex: firstSequenceLength),
+            GetSumBetween(secondSequence,
+                startIndex: indexSummedThroughForSecondSequence + 1,
+                endIndex: secondSequenceLength));
+
+        return maximumSum;
+    }
+
+    private static int GetSumBetween(int[] sequence, int startIndex, int endIndex)
+    {
+        int sum = 0;
+        for (int i = startIndex; i <= endIndex; ++i)
+        {
+            sum += sequence[i];
+        }
+
+        return sum;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int[] firstSequence;
+        int[] secondSequence;
+
+        while ((firstSequence = Array.ConvertAll(
+            Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+            int.Parse)).Length != 1)
+        {
+            secondSequence = Array.ConvertAll(
+                Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+                int.Parse);
+
+            Console.WriteLine(
+                ANARC05B.Solve(firstSequence, secondSequence));
+        }
+    }
+}
