2bf03fd72818a4f0e54450a331760d98a6eeb848
David Galehouse <davghouse@gmail.com>
2016-10-11 01:29:43 -0400

    FIBOSUM

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -1,29 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/PPATH/
+// Currently verifying for: http://www.spoj.com/problems/FIBOSUM/
 public static class InputVerifier
 {
     private static void Main()
     {
         int remainingTestCases = int.Parse(Console.ReadLine());
-        if (remainingTestCases < 1 || remainingTestCases > 100)
-            throw new FormatException();
 
         while (remainingTestCases-- > 0)
         {
-            int[] primes = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] range = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int rangeStart = range[0];
+            int rangeEnd = range[1];
 
-            if (primes[0] < 1000 || primes[0] > 9999)
+            if (rangeStart > rangeEnd)
                 throw new FormatException();
 
-            if (primes[1] < 1000 || primes[1] > 9999)
+            if (rangeEnd > 100000)
                 throw new FormatException();
-
-            // I'll trust they're primes.
         }
     }
 }
---
+++ Spoj.Solver/Solutions/6_Emperor/FIBOSUM.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// 8001 http://www.spoj.com/problems/FIBOSUM/ Fibonacci Sum
+// Finds the sum of the fibonacci numbers in a range.
+public static class FIBOSUM
+{
+    private const int _mod = 1000000007;
+    private const int _precomputedLimit = 1000;
+    private static readonly int[] _precomputedFibNums = new int[_precomputedLimit + 1];
+    private static readonly Dictionary<int, int> _fibNumMemoizer = new Dictionary<int, int>();
+
+    static FIBOSUM()
+    {
+        _precomputedFibNums[0] = 0;
+        _precomputedFibNums[1] = 1;
+
+        for (int i = 2; i <= _precomputedLimit; ++i)
+        {
+            _precomputedFibNums[i] = (_precomputedFibNums[i - 1] + _precomputedFibNums[i - 2]) % _mod;
+        }
+    }
+
+    // https://en.wikipedia.org/wiki/Fibonacci_number
+    // https://en.wikipedia.org/wiki/Modular_arithmetic
+    // http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
+    // Lots of formulas on the Wikipedia page for Fibonacci numbers. One of them
+    // tells us fib sum 1 to n equals fib(n + 2) - 1, so we just need to calculate
+    // a single really large Fibonacci number. There's a form with phi to a power
+    // but there's no way to mod intermediate results (necessary, so they fit into a C# data type)
+    // when we're dealing with non-integers. Normal fib calculation will be way too slow,
+    // but somewhere in the article there's two formulas for fib n in terms of n / 2, so
+    // it'll be log(n) with memoization and it allows us to mod intermediate results so
+    // we'll be dealing with at most int64s in the intermediate (squaring 1 billion and 7 fits into long!).
+    // F(2n - 1) = F(n)^2 + F(n - 1)^2, F(2n) = (2F(n - 1) + F(n))F(n), mods applied liberally.
+    // Throughout what follows I say 'fib nums' but I really mean 'fib nums mod 1 billion and 7.'
+    public static int Solve(int rangeStart, int rangeEnd)
+    {
+        // Sum through the end, minus sum up to the start that we shouldn't have counted:
+        int rangeSum = GetFibNum(rangeEnd + 2) - GetFibNum((rangeStart - 1) + 2);
+
+        // The above result could've been negative if the 2nd term was larger (due to mods),
+        // but what we want is the range sum and that should always be positive after mods, so adjust.
+        return rangeSum >= 0 ? rangeSum : rangeSum + _mod;
+    }
+
+    private static int GetFibNum(int n)
+    {
+        if (n <= _precomputedLimit)
+            return _precomputedFibNums[n];
+
+        int nFibNum;
+        if (_fibNumMemoizer.TryGetValue(n, out nFibNum))
+            return nFibNum;
+
+        if (n % 2 == 0)
+        {
+            long nDiv2Minus1FibNum = GetFibNum(n / 2 - 1);
+            long nDiv2FibNum = GetFibNum(n / 2);
+            nFibNum = (int)(((((2 * nDiv2Minus1FibNum % _mod) + nDiv2FibNum) % _mod) * nDiv2FibNum) % _mod);
+        }
+        else // n % 2 == 1
+        {
+            long nPlus1Div2FibNum = GetFibNum((n + 1) / 2);
+            long nDiv2FibNum = GetFibNum(n / 2);
+            nFibNum = (int)((nPlus1Div2FibNum * nPlus1Div2FibNum % _mod + nDiv2FibNum * nDiv2FibNum % _mod) % _mod);
+        }
+
+        _fibNumMemoizer[n] = nFibNum;
+        return nFibNum;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+        var output = new StringBuilder();
+
+        while (remainingTestCases-- > 0)
+        {
+            int[] range = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            output.Append(FIBOSUM.Solve(range[0], range[1]));
+            output.AppendLine();
+        }
+
+        Console.Write(output);
+    }
+}
