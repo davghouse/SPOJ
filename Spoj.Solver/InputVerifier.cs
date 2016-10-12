1942134941d1684411b6081e326566c754e72e9d
David Galehouse <davghouse@gmail.com>
2016-10-11 22:01:29 -0400

    ENIGMATH

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -1,25 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/FIBOSUM/
+// Currently verifying for: http://www.spoj.com/problems/ENIGMATH/ (it's improperly formatted?).
+// Weird, got a runtime exception solving but verifier didn't. Doesn't get run against all inputs?
 public static class InputVerifier
 {
     private static void Main()
     {
         int remainingTestCases = int.Parse(Console.ReadLine());
 
+        if (remainingTestCases < 0 || remainingTestCases > 1000)
+            throw new FormatException();
+
         while (remainingTestCases-- > 0)
         {
-            int[] range = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-            int rangeStart = range[0];
-            int rangeEnd = range[1];
+            int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-            if (rangeStart > rangeEnd)
+            if (ab.Length != 2)
                 throw new FormatException();
 
-            if (rangeEnd > 100000)
+            if (ab.Any(n => n < 1 || n > 1000000000))
                 throw new FormatException();
         }
     }
---
+++ Spoj.Solver/Solutions/4_Prince/ENIGMATH.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+
+// 15965 http://www.spoj.com/problems/ENIGMATH/ PLAY WITH MATH
+// Solves Ax - By = 0 with x and y the smallest possible positive integers.
+public static class ENIGMATH
+{
+    // Ax = By => y = (A/B)x. A and B might not be in lowest terms, but we
+    // can divide each by their GCD so that they are. So assume they are in
+    // lowest terms, A', B'. y must be an integer, so x has to be a multiple of B'
+    // to make (A'/B')x an integer (since B' has no factors in common with A').
+    // Choosing x = B' then minimizes y as A', and also minimizes x. Therefore,
+    // x = B' = B / GCD(A,B) and y = A' = A / GCD(A,B). Consuming the array for convenience.
+    public static int[] Solve(int[] ab)
+    {
+        int a = ab[0];
+        int b = ab[1];
+        int gcd = GreatestCommonDivisor(a, b);
+        ab[0] = b / gcd; // mapping to x
+        ab[1] = a / gcd; // mapping to y
+
+        return ab; // returning xy
+    }
+
+    // This is a good article (first section): http://www.cut-the-knot.org/blue/Euclid.shtml.
+    // One point to note, for a = bt + r, the gcd(a, b) divides a so it divides bt + r.
+    // And it divides b, so it divides bt, which means for bt + r to be divisible by it,
+    // r also needs to be divisible by it. So it divides both b and r. And the article
+    // notes the importance of showing not only does it divide b and r, it's also their gcd.
+    public static int GreatestCommonDivisor(int a, int b)
+    {
+        int temp;
+        while (b != 0)
+        {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+        var output = new StringBuilder();
+
+        while (remainingTestCases-- > 0)
+        {
+            int[] ab = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            int[] xy = ENIGMATH.Solve(ab);
+
+            output.Append(
+                $"{xy[0]} {xy[1]}");
+            output.AppendLine();
+        }
+
+        Console.WriteLine(output);
+    }
+}
