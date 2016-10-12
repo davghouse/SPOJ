447ae6747ba75f5e325166f8cbd5673b1a9ddc15
David Galehouse <davghouse@gmail.com>
2016-10-12 00:49:15 -0400

    PIR

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -4,25 +4,24 @@ using System.Linq;
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/ENIGMATH/ (it's improperly formatted?).
-// Weird, got a runtime exception solving but verifier didn't. Doesn't get run against all inputs?
+// Currently verifying for: http://www.spoj.com/problems/PIR/
 public static class InputVerifier
 {
     private static void Main()
     {
         int remainingTestCases = int.Parse(Console.ReadLine());
 
-        if (remainingTestCases < 0 || remainingTestCases > 1000)
+        if (remainingTestCases < 0 || remainingTestCases > 100000)
             throw new FormatException();
 
         while (remainingTestCases-- > 0)
         {
-            int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int[] wvuUVW = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-            if (ab.Length != 2)
+            if (wvuUVW.Length != 6)
                 throw new FormatException();
 
-            if (ab.Any(n => n < 1 || n > 1000000000))
+            if (wvuUVW.Any(n => n < 1 || n > 1000))
                 throw new FormatException();
         }
     }
---
+++ Spoj.Solver/Solutions/3_Warlord/PIR.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+// 23 http://www.spoj.com/problems/PIR/ Pyramids
+// Calculates the volume of a triangular pyramid, given its side lengths.
+public static class PIR
+{
+    // See https://en.wikipedia.org/wiki/Tetrahedron#Heron-type_formula_for_the_volume_of_a_tetrahedron.
+    // References a paper talking about a lot of numerical analysis issues, but for
+    // us the main problem will be making sure we line up the input to match the
+    // side lengths needed by the formula. Inspect sides listed by the problem,
+    // compare to U, V, W, u, v, w needed by formula, to see wvuUVW is input order.
+    public static double Solve(int[] wvuUVW)
+    {
+        double U = wvuUVW[3], V = wvuUVW[4], W = wvuUVW[5];
+        double u = wvuUVW[2], v = wvuUVW[1], w = wvuUVW[0];
+
+        double X = (w - U + v) * (U + v + w);
+        double x = (U - v + w) * (v - w + U);
+        double Y = (u - V + w) * (V + w + u);
+        double y = (V - w + u) * (w - u + V);
+        double Z = (v - W + u) * (W + u + v);
+        double z = (W - u + v) * (u - v + W);
+
+        double a = Math.Sqrt(x * Y * Z);
+        double b = Math.Sqrt(y * Z * X);
+        double c = Math.Sqrt(z * X * Y);
+        double d = Math.Sqrt(x * y * z);
+
+        double termUnderSqrt = (-a + b + c + d) * (a - b + c + d) * (a + b - c + d) * (a + b + c - d);
+        double numerator = Math.Sqrt(termUnderSqrt);
+        double denominator = 192 * u * v * w;
+
+        return numerator / denominator;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+        var output = new StringBuilder();
+
+        while (remainingTestCases-- > 0)
+        {
+            int[] wvuUVW = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            output.AppendLine(
+                PIR.Solve(wvuUVW).ToString("F4"));
+        }
+
+        Console.WriteLine(output);
+    }
+}
