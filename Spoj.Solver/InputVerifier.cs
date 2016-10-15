c1f64503ba5adcf3f09d2b5f6bc7b7760dfc28c3
David Galehouse <davghouse@gmail.com>
2016-10-15 15:50:37 -0400

    AMR12D

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/GLJIVE/
+// Currently verifying for: http://www.spoj.com/problems/AMR12D/
 public static class InputVerifier
 {
     private static void Main()
     {
-        int[] points = new int[10];
+        int remainingTestCases = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < 10; ++i)
+        while (remainingTestCases-- > 0)
         {
-            points[i] = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
 
-            if (points[i] < 0 || points[i] > 100)
+            if (s.Any(c => c < 'a' || c > 'z'))
                 throw new FormatException();
         }
     }
---
+++ Spoj.Solver/Solutions/4 - Prince/AMR12D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// 13043 http://www.spoj.com/problems/AMR12D/ The Mirror of Galadriel
+// Determines if the reverse of every substring exists in a string.
+public static class AMR12D
+{
+    // One substring is the entire string. For the reverse of the entire string
+    // to be in the string, the reverse would need to be the same as the string,
+    // since the string is the only substring long enough to match the reversed string.
+    // But given it's a palindrome, the reverse of all other substrings also exist.
+    // To see this, just imagine flipping a substring across the midpoint, the match
+    // always there on the other side (but in reverse) since the string is the same
+    // forwards and backwards.
+    public static bool Solve(string s)
+    {
+        for (int i = 0, j = s.Length - 1; i < j; ++i, --j)
+        {
+            if (s[i] != s[j])
+                return false;
+        }
+
+        return true;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+
+        while (remainingTestCases-- > 0)
+        {
+            Console.WriteLine(
+                AMR12D.Solve(Console.ReadLine()) ? "YES" : "NO");
+        }
+    }
+}
