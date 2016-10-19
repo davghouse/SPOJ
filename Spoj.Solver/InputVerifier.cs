3cd17424b4e81e8b8a97d04ebc3d13f7c8c50123
David Galehouse <davghouse@gmail.com>
2016-10-19 01:43:46 -0400

    CPRMT

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -4,32 +4,22 @@ using System.Linq;
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/MISERMAN/ (it's improperly formatted)
+// Currently verifying for: http://www.spoj.com/problems/CPRMT/
 public static class InputVerifier
 {
     private static void Main()
     {
-        string[] line = Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
-        int cityCount = int.Parse(line[0]);
-        int busCount = int.Parse(line[1]);
+        string a, b;
 
-        if (cityCount < 1 || cityCount > 100)
-            throw new FormatException();
-
-        if (busCount < 1 || busCount > 100)
-            throw new FormatException();
-
-        for (int c = 0; c < cityCount; ++c)
+        while ((a = Console.ReadLine()) != null)
         {
-            line = Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries);
+            b = Console.ReadLine();
 
-            for (int b = 0; b < busCount; ++b)
-            {
-                int busFare = int.Parse(line[b]);
+            if (a.Any(c => c < 'a' || c > 'z'))
+                throw new FormatException();
 
-                if (busFare < 1 || busFare > 100)
-                    throw new FormatException();
-            }
+            if (b.Any(c => c < 'a' || c > 'z'))
+                throw new FormatException();
         }
     }
 }
---
+++ Spoj.Solver/Solutions/4 - Prince/CPRMT.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+// 1728 http://www.spoj.com/problems/CPRMT/ Common Permutation
+// From two strings finds the longest string with some permutation being a subsequence of both.
+public static class CPRMT
+{
+    // Note the subsequence doesn't have to be contiguous. Since we can take any permutation of
+    // the string, the string from all common characters between the two strings is the
+    // best possible. Each string has all the characters, so in whatever order they appear
+    // defines the necessary permutation. We can't do any better, since if the characters
+    // aren't in common between the two, at least one doesn't have all the characters and
+    // therefore no subsequence containing those characters could exist. The problem
+    // says if there's more than one x print in alphabetical order, and of course there will be
+    // if they have more than one unique character in common, since any permutation of x is an x.
+    public static string Solve(string a, string b)
+    {
+        var x = new StringBuilder();
+
+        // Gonna take some care to do this in a single pass, rather than 26 passes with LINQ's Count().
+        // We know they're lowercase so buckets are convenient. For unbucketable we might need to sort both,
+        // then emulate a merge while counting up, or O(n) w/ overhead from a HashSet may be worth it.
+        int[] aCharacterCounts = new int[26];
+        int[] bCharacterCounts = new int[26];
+
+        foreach (char c in a)
+        {
+            ++aCharacterCounts[c - 'a'];
+        }
+
+        foreach (char c in b)
+        {
+            ++bCharacterCounts[c - 'a'];
+        }
+
+        for (char c = 'a'; c <= 'z'; ++c)
+        {
+            x.Append(c, Math.Min(aCharacterCounts[c - 'a'], bCharacterCounts[c - 'a']));
+        }
+
+        return x.ToString();
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        string a, b;
+        var output = new StringBuilder();
+
+        while ((a = Console.ReadLine()) != null)
+        {
+            b = Console.ReadLine();
+
+            output.AppendLine(
+                CPRMT.Solve(a, b));
+        }
+
+        Console.Write(output);
+    }
+}
