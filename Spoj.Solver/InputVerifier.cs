85d6c962e7ec97b90625d148b8f28bad159a351f
David Galehouse <davghouse@gmail.com>
2016-11-12 16:49:59 -0500

    BYECAKES

--- Spoj.Solver/InputVerifier.cs
+++ Spoj.Solver/InputVerifier.cs
@@ -4,25 +4,30 @@ using System.Linq;
 // Too many problems have improperly formatted input... random whitespace,
 // missing newlines... things that C# doesn't deal well with by default.
 // It's good to verify the input format on an alt account so you don't get frustrated.
-// Currently verifying for: http://www.spoj.com/problems/DIEHARD/
+// Currently verifying for: http://www.spoj.com/problems/BYECAKES/ (it's improperly formatted)
 public static class InputVerifier
 {
     private static void Main()
     {
-        int remainingTestCases = int.Parse(Console.ReadLine());
-
-        if (remainingTestCases < 1 || remainingTestCases > 10)
-            throw new FormatException();
-
-        while (remainingTestCases-- > 0)
+        int[] line;
+        while ((line = Array.ConvertAll(
+            Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+            int.Parse))[0] != -1)
         {
-            int[] line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-            if (line[0] < 1 || line[0] > 1000)
+            if (line.Length != 8)
                 throw new FormatException();
 
-            if (line[1] < 1 || line[1] > 1000)
-                throw new FormatException();
+            for (int i = 0; i < 4; i++)
+            {
+                if (line[i] < 0 || line[i] > 1000)
+                    throw new FormatException();
+            }
+
+            for (int i = 4; i < line.Length; i++)
+            {
+                if (line[i] < 1 || line[i] > 1000)
+                    throw new FormatException();
+            }
         }
     }
 }
---
+++ Spoj.Solver/Solutions/3 - Warlord/BYECAKES.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+// 7185 http://www.spoj.com/problems/BYECAKES/ Bye Bye Cakes
+// Finds the minimal amount of cake ingredients to buy to use all of them completely up.
+public static class BYECAKES
+{
+    // Find the greatest number of cakes needed to deplete any one of the starting ingredients.
+    // This is the total number of cakes to bake, buying (across each ingredient) any missing
+    // ingredients necessary to support that number of cakes. Pretty clear that it's minimal.
+    // To find the amount to buy, subtract the amount owned from the amount needed.
+    public static int[] Solve(
+        int eggsOwned, int flourOwned, int sugarOwned, int milkOwned,
+        int eggsPerCake, int flourPerCake, int sugarPerCake, int milkPerCake)
+    {
+        int totalCakesToBake = new[]
+        {
+            GetNumberOfCakesToDepleteIngredient(eggsOwned, eggsPerCake),
+            GetNumberOfCakesToDepleteIngredient(flourOwned, flourPerCake),
+            GetNumberOfCakesToDepleteIngredient(sugarOwned, sugarPerCake),
+            GetNumberOfCakesToDepleteIngredient(milkOwned, milkPerCake)
+        }.Max();
+
+        return new[]
+        {
+            GetAmountOfIngredientsToBuy(totalCakesToBake, eggsOwned, eggsPerCake),
+            GetAmountOfIngredientsToBuy(totalCakesToBake, flourOwned, flourPerCake),
+            GetAmountOfIngredientsToBuy(totalCakesToBake, sugarOwned, sugarPerCake),
+            GetAmountOfIngredientsToBuy(totalCakesToBake, milkOwned, milkPerCake)
+        };
+    }
+
+    private static int GetNumberOfCakesToDepleteIngredient(int ingredientsOwned, int ingredientsPerCake)
+        => ingredientsOwned % ingredientsPerCake == 0
+        ? ingredientsOwned / ingredientsPerCake
+        : ingredientsOwned / ingredientsPerCake + 1;
+
+    private static int GetAmountOfIngredientsToBuy(int totalCakesToBake, int ingredientsOwned, int ingredientsPerCake)
+        => totalCakesToBake * ingredientsPerCake - ingredientsOwned;
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int[] line;
+        while ((line = Array.ConvertAll(
+            Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+            int.Parse))[0] != -1)
+        {
+            int[] ingredientsToBuy = BYECAKES.Solve(
+                line[0], line[1], line[2], line[3],
+                line[4], line[5], line[6], line[7]);
+
+            Console.WriteLine(
+                string.Join(" ", ingredientsToBuy));
+        }
+    }
+}
