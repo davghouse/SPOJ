ad9dbeb546963bc05db615688372ca9098e69e3a
David Galehouse <davghouse@gmail.com>
2019-02-03 18:12:53 -0500

    Shorten line length of some comments

--- Spoj.Solver/Solutions/4 - Prince/AE00.cs
+++ Spoj.Solver/Solutions/4 - Prince/AE00.cs
@@ -1,15 +1,18 @@
 using System;
 
 // https://www.spoj.com/problems/AE00/ #division #experiment #math
-// Counts the number of rectangles that can be constructed from n 1x1 squares (using any number of those squares), 1 <= n <= 10000.
-// More info here: http://mathschallenge.net/library/number/number_of_divisors, https://en.wikipedia.org/wiki/Divisor_function
+// Counts the number of rectangles that can be constructed from n 1x1 squares
+// (using any number of those squares), 1 <= n <= 1000. More info here:
+// http://mathschallenge.net/library/number/number_of_divisors
+// https://en.wikipedia.org/wiki/Divisor_function
 public static class AE00
 {
     public static int Solve(int n)
     {
-        // Rectangles from n squares can use any number of squares, that is, n squares can make (recursively)
-        // as many rectangles as n - 1 squares make, plus however many rectangles from exactly n squares.
-        // So we'll keep a cumulative count of rectangles made from s = 1 squares up to s = n squares.
+        // Rectangles from n squares can use any number of squares, that is, n squares
+        // can make (recursively) as many rectangles as n - 1 squares make, plus however
+        // many rectangles from exactly n squares. So we'll keep a cumulative count of
+        // rectangles made from s = 1 squares up to s = n squares.
         int cumulativeRectangleCount = 0;
         for (int s = 1; s <= n; ++s)
         {
