94516f10e3c26cde75ac59e86e7db05e2eae514a
David Galehouse <davghouse@gmail.com>
2019-02-03 12:56:03 -0500

    TEMPLATE4

---
+++ Spoj.Solver/Solutions/TEMPLATE4.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+// https://www.spoj.com/problems/TEMPLATE4/ <tags>
+// <description>
+public static class TEMPLATE4
+{
+    public static int Solve(int a, int b)
+        => a * b;
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        var output = new StringBuilder();
+        int remainingTestCases = FastIO.ReadNonNegativeInt();
+        while (remainingTestCases-- > 0)
+        {
+            int a = FastIO.ReadNonNegativeInt();
+            int b = FastIO.ReadNonNegativeInt();
+
+            output.Append(
+                TEMPLATE4.Solve(a, b));
+            output.AppendLine();
+        }
+
+        Console.Write(output);
+    }
+}
+
+// This is based in part on submissions from https://www.codechef.com/status/INTEST.
+// It's assumed the input is well-formed, so if you try to read an integer when no
+// integers remain in the input, there's undefined behavior (infinite loop).
+public static class FastIO
+{
+    private const byte _null = (byte)'\0';
+    private const byte _newLine = (byte)'\n';
+    private const byte _minusSign = (byte)'-';
+    private const byte _zero = (byte)'0';
+    private const int _inputBufferLimit = 8192;
+
+    private static readonly Stream _inputStream = Console.OpenStandardInput();
+    private static readonly byte[] _inputBuffer = new byte[_inputBufferLimit];
+    private static int _inputBufferSize = 0;
+    private static int _inputBufferIndex = 0;
+
+    private static byte ReadByte()
+    {
+        if (_inputBufferIndex == _inputBufferSize)
+        {
+            _inputBufferIndex = 0;
+            _inputBufferSize = _inputStream.Read(_inputBuffer, 0, _inputBufferLimit);
+            if (_inputBufferSize == 0)
+                return _null; // All input has been read.
+        }
+
+        return _inputBuffer[_inputBufferIndex++];
+    }
+
+    public static int ReadNonNegativeInt()
+    {
+        byte digit;
+
+        // Consume and discard whitespace characters (their ASCII codes are all < _minusSign).
+        do
+        {
+            digit = ReadByte();
+        }
+        while (digit < _minusSign);
+
+        // Build up the integer from its digits, until we run into whitespace or the null byte.
+        int result = digit - _zero;
+        while (true)
+        {
+            digit = ReadByte();
+            if (digit < _zero) break;
+            result = result * 10 + (digit - _zero);
+        }
+
+        return result;
+    }
+}
